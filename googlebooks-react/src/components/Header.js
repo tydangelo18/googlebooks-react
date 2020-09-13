43b7d1e67c520549fc06e3f8098b99671e59a6aa
Ty D'Angelo <tydangelo18@gmail.com>
2020-09-13 12:07:04 -0500

    create-react-app, built folder structure, created books and saved books pages, built Header component.

---
+++ googlebooks-react/src/components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../styles/Header.css';
+
+class Header extends Component {
+    render() {
+        return (
+            <header>
+                <h1>Google Books</h1>
+            </header>
+        )
+    }
+}
+
+export default Header;
---
+++ googlebooks-react/src/pages/BooksPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+// Import Components 
+import Header from '../components/Header';
+
+class BooksPage extends Component {
+    render() {
+        return (
+            <div className="BooksPage">
+                <Header />
+            </div>
+        );
+    }
+}
+
+export default BooksPage;
