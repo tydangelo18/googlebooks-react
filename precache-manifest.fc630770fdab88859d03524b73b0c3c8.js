68ca9763352dd0579388bf9d74c256a8db2b7c79
Ty D'Angelo <tydangelo18@gmail.com>
2020-09-15 16:44:07 -0500

    Updates

---
+++ precache-manifest.fc630770fdab88859d03524b73b0c3c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55da4f9c0b4e6bddf3248bb021aedfe4",
+    "url": "/googlebooks-react/index.html"
+  },
+  {
+    "revision": "d5d105fd1639a7215936",
+    "url": "/googlebooks-react/static/css/main.3eea5e96.chunk.css"
+  },
+  {
+    "revision": "f193c612483bdb258aae",
+    "url": "/googlebooks-react/static/js/2.31407046.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/googlebooks-react/static/js/2.31407046.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5d105fd1639a7215936",
+    "url": "/googlebooks-react/static/js/main.59fa4423.chunk.js"
+  },
+  {
+    "revision": "05974ae50799a5413e31",
+    "url": "/googlebooks-react/static/js/runtime-main.4a107bfd.js"
+  }
+]);
