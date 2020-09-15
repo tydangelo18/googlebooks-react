a0f408c2e517b9e50f6f2301cd69232b646c1686
Ty D'Angelo <tydangelo18@gmail.com>
2020-09-15 12:13:25 -0500

    Styled Header.

--- googlebooks-react/src/components/Header.js
+++ googlebooks-react/src/components/Header.js
@@ -4,9 +4,19 @@ import '../styles/Header.css';
 class Header extends Component {
     render() {
         return (
-            <header>
-                <h1>Google Books</h1>
-            </header>
+            <div className="header">
+                <header>
+                    <h1 className="title">Google Books</h1>
+                </header>
+                <div className="navItems">
+                        <div className="search" style={{paddingRight: "10px"}}>
+                            <h3 className="searchNav">Search</h3>
+                        </div>
+                        <div className="saved">
+                            <h3 className="savedNav">Saved</h3>
+                        </div>
+                </div>
+            </div>
         )
     }
 }
--- googlebooks-react/src/pages/SavedBooks.js
+++ googlebooks-react/src/pages/SavedBooks.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import SavedHeader from '../components/SavedHeader';
+import Header from '../components/SavedHeader';
 import Saved from '../components/Saved';
 
 class SavedBooks extends Component {
     render() {
         return (
             <div className="BooksPage">
-                <SavedHeader />  
+                <Header />  
                 <Saved />  
             </div>
         );
